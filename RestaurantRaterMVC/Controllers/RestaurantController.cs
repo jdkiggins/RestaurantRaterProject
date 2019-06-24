386ab1f673b850ecc97cefa2d4d7c86f63462756
jdkiggins <jdkiggins@outlook.com>
2019-06-24 15:32:32 -0400

    did more things

---
+++ RestaurantRaterMVC/Controllers/RestaurantController.cs
@@ -0,0 +1,69 @@
+﻿using RestaurantRaterMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RestaurantRaterMVC.Controllers
+{
+    public class RestaurantController : Controller
+    {
+        private RestaurantDbContext _db = new RestaurantDbContext();
+
+        // GET: Restaurant
+        public ActionResult Index()
+        {
+            return View(_db.Restaurants.ToList());
+        }
+
+        //GET : Resturant/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //POST : Restaurant/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Restaurants.Add(restaurant);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(restaurant);
+        }
+
+        //get Restaurant/Delete/{id}
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            if (restaurant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurant);
+        }
+
+        //Post : Restaurant/Delete/{id}
+        [HttpPost]
+        [ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            _db.Restaurants.Remove(restaurant);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
--- RestaurantRaterMVC/Models/Restaurant.cs
+++ RestaurantRaterMVC/Models/Restaurant.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,18 @@ namespace RestaurantRaterMVC.Models
 {
     public class Restaurant
     {
+        public int RestaurantID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        public double Rating { get; set; }
+
+        [Display(Name="Food Type")]
+        public string FoodType { get; set; }
+    }
+
+    public class RestaurantDbContext : DbContext
+    {
+        public DbSet<Restaurant> Restaurants { get; set; }
     }
 }
